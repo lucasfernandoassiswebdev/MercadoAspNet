efc2fffd15daefb03e750c45404cbc81cc0c6348
Lucas Fernando <lucasaassis@outlook.com>
2017-08-16 15:47:25 -0300

    Tela de login adicionada e funcionando

--- MercadoMain/Controllers/HomeController.cs
+++ MercadoMain/Controllers/HomeController.cs
@@ -1,12 +1,57 @@
-﻿using System.Web.Mvc;
-using System.Web.UI.WebControls;
+﻿using System;
+using System.Web.Mvc;
+using MercadoAplicacao.LoginApp;
+using MercadoDominio.Entidades;
 
 namespace ProjetoMercado.Controllers
 {
     public class HomeController : Controller
     {
+        private LoginAplicacao appLogin; 
+
+        public HomeController()
+        {
+            appLogin = LoginAplicacaoConstrutor.LoginAplicacaoADO();
+        }
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                var logins = appLogin.ListarTodos();
+
+                foreach (var log in logins)
+                {
+                    if (log.LoginU == login.LoginU && log.Senha == login.Senha)
+                    {
+                        Session["Login"] = login.LoginU;
+                        ViewBag.Resultado = Session["Login"].ToString();
+                    }
+                }
+                return RedirectToAction("LogadoComSucesso");
+            }
+            return View(login);
+        }
+
+        public ActionResult LogadoComSucesso()
+        {
+            if (Session["Login"] == null)
+            {
+                @ViewBag.Resultado = "login ou senha incorretos";
+            }
+            else
+            {
+                @ViewBag.Resultado = Session["Login"] + " logado com sucesso";
+                Session["Login"] = "";
+            }
+
             return View();
         }
     }
