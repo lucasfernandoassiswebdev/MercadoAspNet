c3368dc5ce781457b1284f7ec7cdfbfb106d8178
LUCAS FERNANDO DE ASSIS <lucasaassis@outlook.com>
2017-08-27 19:35:21 -0300

    começando a adicionar simple injector no projeto

--- MercadoMain/Global.asax.cs
+++ MercadoMain/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using MercadoMain.SimpleInjector;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -8,6 +9,7 @@ namespace MercadoMain
     {
         protected void Application_Start()
         {
+            DependencyResolver.SetResolver(SimpleInjectorContainer.RegisterServices());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ MercadoMain/SimpleInjector/SimpleInjectorContainer.cs
@@ -0,0 +1,19 @@
+﻿using Mercado.Aplicacao.DistribuidorApp;
+using SimpleInjector;
+
+namespace MercadoMain.SimpleInjector
+{
+    public static class SimpleInjectorContainer
+    {
+        public static Container RegisterServices()
+        {
+            var container = new Container();
+
+            //registrando as implementações
+            container.Register<DistribuidorAplicacaoConstrutor, DistribuidorAplicacao>();
+
+            container.Verify();
+            return container;
+        }
+    }
+}
